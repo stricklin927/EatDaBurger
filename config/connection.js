e7e2df3b47443f43feabd74aa5e34cee14b6dd6f
Alexandra Stricklin <stricklin927@gmail.com>
2020-12-19 22:27:04 -0500

    edit connection and orm files

---
+++ config/connection.js
@@ -0,0 +1,18 @@
+// conncetion to db
+var mysql = require("mysql");
+connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "rootroot",
+    database: "burgers_db"
+});
+
+connection.connect(function(err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+
+module.exports = connection;
