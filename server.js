c0e54dac6c84c93dc2aa194ed4237654dded8156
Alexandra Stricklin <stricklin927@gmail.com>
2020-12-19 19:58:19 -0500

    edit server.js

--- server.js
+++ server.js
@@ -7,5 +7,12 @@ var PORT = process.env.PORT || 3000;
 
 app.listen(PORT, function() {
     console.log("Server listening on: http://localhost:" + PORT);
-
 });
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
