383c1787794fbadc9c9005823235023d70a81ea5
Alexandra Stricklin <stricklin927@gmail.com>
2020-12-22 04:03:33 -0500

    Heroku connection

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,19 @@
 // conncetion to db
 var mysql = require("mysql");
-connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "rootroot",
-    database: "burgers_db"
-});
+var connection;
+
+// JAWS DB
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "rootroot",
+        database: "burgers_db"
+    });        
+};
+
 
 connection.connect(function(err) {
     if (err) {
